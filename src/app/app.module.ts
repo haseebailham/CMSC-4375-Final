0d9a07a503d45bef2df5d158fc228195a6b20dc9
b2333981 <b2333981@ben.edu>
2019-11-06 10:49:24 -0600

    added new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+import { RecipesPageComponent } from './recipes-page/recipes-page.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipeFormComponent,
+    ContactPageComponent,
+    RecipesPageComponent,
+    FaqPageComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule
