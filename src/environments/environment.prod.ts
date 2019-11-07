52fed70146b4f9179394c0aed1497b8d83569af8
Kayla McHale <kayla_mchale@ben.edu>
2019-11-07 11:53:47 -0600

    Added Firebase credentials

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
+
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDwviL5mdCJmWEgwQEVeRtaLIMvc7KQwk8',
+    authDomain: 'cmsc-4375-lab-4.firebaseapp.com',
+    databaseURL: 'https://cmsc-4375-lab-4.firebaseio.com',
+    projectId: 'cmsc-4375-lab-4',
+    storageBucket: 'cmsc-4375-lab-4.appspot.com',
+    messagingSenderId: '346345049685'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,17 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDwviL5mdCJmWEgwQEVeRtaLIMvc7KQwk8',
+    authDomain: 'cmsc-4375-lab-4.firebaseapp.com',
+    databaseURL: 'https://cmsc-4375-lab-4.firebaseio.com',
+    projectId: 'cmsc-4375-lab-4',
+    storageBucket: 'cmsc-4375-lab-4.appspot.com',
+    messagingSenderId: '346345049685'
+  }
 };
 
 /*
