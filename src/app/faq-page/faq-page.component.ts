789175c5a435b3fc543eacd0876f423b7628c50d
Kayla McHale <kayla_mchale@ben.edu>
2019-11-13 16:37:46 -0600

    Updated faq-page.component.ts file

--- src/app/faq-page/faq-page.component.ts
+++ src/app/faq-page/faq-page.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormControl, FormGroup} from '@angular/forms';
+import {Router} from "@angular/router";
+// import {Service} from '../service';
 
 @Component({
   selector: 'app-faq-page',
-  templateUrl: './faq-page.component.html',
-  styleUrls: ['./faq-page.component.css']
+  templateUrl: './faq.component.html',
+  styleUrls: ['./faq.component.css']
 })
-export class FaqPageComponent implements OnInit {
-
-  constructor() { }
+export class FaqComponent implements OnInit {
+  questionForm: FormGroup;
+  private questionList;
+  constructor(private formBuilder: FormBuilder,) {}
+              // public faqService: Service) { }
 
   ngOnInit() {
+    this.questionForm = this.formBuilder.group({
+      name: [''],
+      email: [''],
+      question: ['']
+    });
+    // @ts-ignore
+    this.questionList = this.faqService.getQuestions().subscribe(res => (this.questionList = res));
   }
-
+  readyForNextQuestion() {
+    this.questionForm = this.formBuilder.group({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      question: new FormControl(''),
+    });
+  }
+  // onClickAddQuestion(questionValue) {
+  //   this.faqService.createQuestions(questionValue).then(r => {
+  //     this.readyForNextQuestion();
+  //   });
+  // }
 }
