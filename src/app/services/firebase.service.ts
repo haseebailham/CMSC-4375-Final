4ed30eceb57beb99f486149da589f50a7dc596c6
b2333981 <b2333981@ben.edu>
2019-11-13 21:23:48 -0600

    crud

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,5 +1,7 @@
 import {Injectable} from '@angular/core';
+// @ts-ignore
 import {AngularFirestore} from '@angular/fire/firestore';
+import {Recipe} from '../recipe';
 
 @Injectable({
   providedIn: 'root'
@@ -7,14 +9,29 @@ import {AngularFirestore} from '@angular/fire/firestore';
 
 export class FirebaseService {
   private value: any;
-
+  private likeComment;
+  private likedCount: number;
   constructor(public db: AngularFirestore) {
+  }
+
+  getRecipes() {
+    return this.db.collection('recipes').snapshotChanges();
+  }
+
+  // createRecipe(recipe: Recipe){
+  //   return this.db.collection('recipes').add(recipe);
+  // }
+
+  updateRecipe(recipe: Recipe) {
+    this.likedCount = recipe.likes+1;
+    this.db.doc('recipes/' + recipe.key).set({likes: this.likedCount}, {merge: true});
   }
 
   createRecipe(value) {
     return this.db.collection('recipes').add({
       username: value.username,
       recipeName: value.recipeName,
+      likes: 0
       // description: value.description,
       // foodType: value.foodType,
       // difficulty: value.difficulty,
@@ -23,6 +40,14 @@ export class FirebaseService {
       // cookingTime: parseInt(value.cookingTime),
       // ingredients: value.ingredients,
       // directions: value.directions
+    });
+  }
+
+  createQuestions(questionValue) {
+    return this.db.collection('questions').add({
+      name: questionValue.name,
+      email: questionValue.email,
+      question: questionValue.question
     });
   }
 }
