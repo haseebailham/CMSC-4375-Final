69b9653b6dbffcb8ae0b3d4aeb27c08f3a3eb629
Sebastian Obacz <seba16obacz@gmail.com>
2019-11-13 21:02:27 -0600

    New

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomepageComponent} from './homepage/homepage.component';
-import {FaqPageComponent} from './faq-page/faq-page.component';
+import {FaqComponent} from './faq/faq.component';
 import {LoginComponent} from './login/login.component';
 import {RegisterComponent} from './register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomepageComponent },
-  { path: 'faq', component: FaqPageComponent},
+  { path: 'faq', component: FaqComponent},
   { path: 'register', component: RegisterComponent  },
   { path: 'login', component: LoginComponent }
 ];
