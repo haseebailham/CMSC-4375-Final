bbd3790ea62fe63b51af61f3855c39b9893b97ee
b2333981 <b2333981@ben.edu>
2019-12-02 12:35:21 -0600

    recipe page component

--- src/app/recipe-page/recipe-page.component.ts
+++ src/app/recipe-page/recipe-page.component.ts
@@ -1,19 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import {Recipe} from "../recipe";
+import {FirebaseService} from "../services/firebase.service";
+import {Router, ActivatedRoute, Params } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-recipe-page',
   templateUrl: './recipe-page.component.html',
-  styleUrls: ['./recipe-page.component.css']
+  styleUrls: ['./recipe-page.component.css'],
+  providers: [FirebaseService]
 })
 export class RecipePageComponent implements OnInit {
 
   recipe: Recipe;
-  constructor(recipe: Recipe) {
-    this.recipe = recipe;
-  }
+
+  constructor() { }
 
   ngOnInit() {
+
 
   }
 
